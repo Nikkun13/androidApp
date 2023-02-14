804a1f43360193bee40a745c1548bf3d89e44998
Nicolas Arcieri <nicolasarcieri@gmail.com>
2023-02-14 20:39:43 -0300

    primeraEntregaV1.6

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import { Button, Image, StyleSheet, Text, View } from "react-native";
+import { Button, Image, Text, View } from "react-native";
 
 import { StatusBar } from "expo-status-bar";
 import { styles } from "./styles";
