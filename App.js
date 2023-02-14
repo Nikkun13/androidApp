c73b492bea9951f74a6feb392e8ae25ef49b6e2c
Nicolas Arcieri <nicolasarcieri@gmail.com>
2023-02-14 20:36:14 -0300

    primeraEntregaV1.5

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
-import { Image, StyleSheet, Text, View } from "react-native";
+import { Button, Image, StyleSheet, Text, View } from "react-native";
 
 import { StatusBar } from "expo-status-bar";
+import { styles } from "./styles";
 
 export default function App() {
   return (
@@ -11,35 +12,8 @@ export default function App() {
       <Text style={styles.textoDos}>
         Como primera app crearé un simulador de dados.
       </Text>
+      <Button title="Lanza los dados" color="#841584" />
       <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#40CFFF",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  textoUno: {
-    color: "#FF0000",
-    paddingBottom: 20,
-    paddingTop: 10,
-    fontSize: 48,
-    textAlign: "center",
-  },
-  textoDos: {
-    color: "#000",
-    paddingBottom: 10,
-    paddingTop: 50,
-    fontSize: 24,
-    textAlign: "center",
-  },
-  imagen: {
-    height: 300,
-    width: 300,
-    borderRadius: 150,
-  },
-});
---
+++ styles.js
@@ -0,0 +1,29 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#40CFFF",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  textoUno: {
+    color: "#FF0000",
+    paddingBottom: 20,
+    paddingTop: 10,
+    fontSize: 48,
+    textAlign: "center",
+  },
+  textoDos: {
+    color: "#000",
+    paddingBottom: 10,
+    paddingTop: 50,
+    fontSize: 24,
+    textAlign: "center",
+  },
+  imagen: {
+    height: 300,
+    width: 300,
+    borderRadius: 150,
+  },
+});
