ff02c1dff395c762ba429cb24d28cb0214366ba4
Nicolas Arcieri <nicolasarcieri@gmail.com>
2023-02-16 21:06:30 -0300

    PrimeraEntregaV1.7

--- App.js
+++ App.js
@@ -1,14 +1,36 @@
-import { Button, Image, Text, View } from "react-native";
-
+import { Button, Image, Text, TextInput, View } from "react-native";
+import React, { useState, useEffect } from "react";
 import { StatusBar } from "expo-status-bar";
 import { styles } from "./styles";
 
 export default function App() {
+  const [itemText, setItemText] = useState("");
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    console.log("itemText", itemText);
+  }, [itemText]);
+
+  const onChangeText = (text) => {
+    setItemText(text);
+  };
+
   return (
     <View style={styles.container}>
-      <Text style={styles.textoUno}>Hola Coder!</Text>
-      <Text style={styles.textoUno}>Soy Nicolás</Text>
-      <Image style={styles.imagen} source={require("./img/Dados.jpg")} />
+      <View>
+        <Text style={styles.textoUno}>Hola Coder!</Text>
+        <Text style={styles.textoUno}>Soy Nicolás</Text>
+      </View>
+      <View>
+        <Image style={styles.imagen} source={require("./img/Dados.jpg")} />
+      </View>
+      <View>
+        <TextInput
+          placeholder="escriba aqui"
+          onChangeText={onChangeText}
+          value={itemText}
+        />
+      </View>
       <Text style={styles.textoDos}>
         Como primera app crearé un simulador de dados.
       </Text>
