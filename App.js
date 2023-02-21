f3fbfb54d2b33dac7c2fea5d7acdf048ab09fc99
Nicolas Arcieri <nicolasarcieri@gmail.com>
2023-02-21 20:51:49 -0300

    PrimeraEntregaV1.80

--- App.js
+++ App.js
@@ -1,40 +1,203 @@
-import { Button, Image, Text, TextInput, View } from "react-native";
-import React, { useState, useEffect } from "react";
+import {
+  Button,
+  FlatList,
+  Image,
+  Modal,
+  Pressable,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
+import React, { useEffect, useState } from "react";
+
 import { StatusBar } from "expo-status-bar";
 import { styles } from "./styles";
 
 export default function App() {
-  const [itemText, setItemText] = useState("");
-  const [items, setItems] = useState([]);
+  const [bolsa, setBolsa] = useState(true);
+  const [diceNumber, setDiceNumber] = useState("");
+  const [dices, setDices] = useState([]);
+  const [imagenLados, setImagenLados] = useState("");
 
-  useEffect(() => {
-    console.log("itemText", itemText);
-  }, [itemText]);
+  const onChangeNumber = (number) => {
+    setDiceNumber(number);
+  };
 
-  const onChangeText = (text) => {
-    setItemText(text);
+  const addDice = () => {
+    if (
+      diceNumber == 4 ||
+      diceNumber == 6 ||
+      diceNumber == 8 ||
+      diceNumber == 10 ||
+      diceNumber == 12 ||
+      diceNumber == 20
+    ) {
+      switch (diceNumber) {
+        case "4":
+          setImagenLados(require("./img/4ladosdice.png"));
+          break;
+        case "6":
+          setImagenLados(require("./img/6ladosdice.png"));
+          break;
+        case "8":
+          setImagenLados(require("./img/8ladosdice.png"));
+          break;
+        case "10":
+          setImagenLados(require("./img/10ladosdice.png"));
+          break;
+        case "12":
+          setImagenLados(require("./img/12ladosdice.png"));
+          break;
+        case "20":
+          setImagenLados(require("./img/20ladosdice.png"));
+          break;
+
+        default:
+          break;
+      }
+      setDices((oldArry) => [
+        ...oldArry,
+        {
+          id: Date.now(),
+          value: diceNumber,
+          img: imagenLados,
+        },
+      ]);
+      setDiceNumber("");
+      setImagenLados("");
+    } else {
+      alert("Escriba 4, 6, 8, 10, 12 o 20");
+      setDiceNumber("");
+    }
+  };
+
+  const irBolsa = () => {
+    setBolsa(!bolsa);
+    setDices([]);
+  };
+
+  const [selectedDice, setSelectedDice] = useState(null);
+
+  const [modalVisible, setModalVisible] = useState(false);
+
+  const selectDice = (dice) => {
+    setSelectedDice(dice);
+    setModalVisible(true);
+  };
+
+  const removeDice = (id) => {
+    setModalVisible(!modalVisible);
+    setDices((oldArry) => oldArry.filter((item) => item.id !== id));
+    setSelectedDice(null);
   };
 
   return (
     <View style={styles.container}>
       <View>
-        <Text style={styles.textoUno}>Hola Coder!</Text>
-        <Text style={styles.textoUno}>Soy Nicolás</Text>
+        {bolsa ? (
+          <>
+            <View>
+              <Text style={styles.textoUno}>Hola Coder!</Text>
+              <Text style={styles.textoUno}>Soy Nicolás</Text>
+            </View>
+            <View>
+              <Image
+                style={styles.imagen}
+                source={require("./img/Dados.jpg")}
+              />
+            </View>
+            <Text style={styles.textoDos}>
+              Como primera app crearé un simulador de dados.
+            </Text>
+            <Button
+              title="Ir a la Bolsa de Dados"
+              color="#841584"
+              onPress={irBolsa}
+            />
+          </>
+        ) : (
+          <>
+            <View>
+              <Text style={styles.textoUno}>En la bolsa!</Text>
+            </View>
+            <View style={styles.container}>
+              <Text style={styles.textoDos}>
+                Seleccionar número de caras del dado a arrojar
+              </Text>
+              <TextInput
+                style={styles.input}
+                onChangeText={onChangeNumber}
+                value={diceNumber}
+                placeholder="4, 6, 8, 10, 12, o 20"
+                keyboardType="numeric"
+              />
+              <Button title="Agregar dado a la bolsa" onPress={addDice} />
+            </View>
+            <FlatList
+              data={dices}
+              renderItem={(data) => (
+                <Pressable
+                  style={styles.contentList}
+                  onPress={() => {
+                    selectDice(data.item);
+                  }}
+                >
+                  <Image style={styles.imagenDado} source={data.item.img} />
+                  <Text style={styles.textoTres}>{data.item.value}</Text>
+                </Pressable>
+              )}
+              keyExtractor={(item) => item.id.toString()}
+            />
+            <Modal
+              animationType="slide"
+              transparent={true}
+              visible={modalVisible}
+            >
+              <View style={styles.modalContainer}>
+                <View styles={styles.modalTitle}>
+                  <Text
+                    style={{
+                      textAlign: "center",
+                      fontSize: 20,
+                      fontWeight: "bold",
+                    }}
+                  >
+                    Eliminar Dado
+                  </Text>
+                </View>
+                <View styles={styles.modalContent}>
+                  <Text>
+                    ¿Está seguro que desea eliminar el Dado de{" "}
+                    {selectedDice?.value} caras?
+                  </Text>
+                </View>
+                <View styles={styles.modalActions}>
+                  <Button
+                    title="Cancelar"
+                    onPress={() => {
+                      setModalVisible(false);
+                      setSelectedDice(null);
+                    }}
+                  />
+                  <Button
+                    title="Eliminar"
+                    onPress={() => {
+                      removeDice(selectedDice.id);
+                    }}
+                  />
+                </View>
+              </View>
+            </Modal>
+            <View>
+              <Button title="Lanzar Dados" color="#FF8000" />
+            </View>
+            <View>
+              <Button title="Regresar" color="#841584" onPress={irBolsa} />
+            </View>
+          </>
+        )}
       </View>
-      <View>
-        <Image style={styles.imagen} source={require("./img/Dados.jpg")} />
-      </View>
-      <View>
-        <TextInput
-          placeholder="escriba aqui"
-          onChangeText={onChangeText}
-          value={itemText}
-        />
-      </View>
-      <Text style={styles.textoDos}>
-        Como primera app crearé un simulador de dados.
-      </Text>
-      <Button title="Lanza los dados" color="#841584" />
+
       <StatusBar style="auto" />
     </View>
   );
--- styles.js
+++ styles.js
@@ -21,9 +21,61 @@ export const styles = StyleSheet.create({
     fontSize: 24,
     textAlign: "center",
   },
+  textoTres: {
+    color: "#000",
+    fontSize: 16,
+    textAlign: "center",
+  },
   imagen: {
     height: 300,
     width: 300,
     borderRadius: 150,
+  },
+  input: {
+    width: 200,
+    borderBottomColor: "black",
+    borderBottomWidth: 1,
+    fontSize: 16,
+    textAlign: "center",
+  },
+  diceContainer: {
+    marginTop: 30,
+    flex: 1,
+  },
+  dice: {
+    padding: 10,
+    textAlign: "center",
+  },
+  contentList: {
+    padding: 10,
+    borderRadius: 5,
+    backgroundColor: "#ccc",
+    justifyContent: "center",
+  },
+  modalContainer: {
+    height: 400,
+    width: 300,
+    marginTop: 100,
+    alignSelf: "center",
+    backgroundColor: "lime",
+  },
+  modalTitle: {
+    padding: 10,
+    borderRadius: 5,
+  },
+  modalContent: {
+    padding: 10,
+    width: "50%",
+  },
+  modalActions: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+  },
+  imagenDado: {
+    height: 40,
+    width: 40,
+    position: "absolute",
+    left: "50%",
+    marginLeft: -10,
   },
 });
